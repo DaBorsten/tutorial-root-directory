3225a37f68fafda4c45750f159b25318d75f79c7
Boas Rautmann <boas_rautmann@web.de>
2023-11-30 14:47:17 +0100

    mitigations

--- cpapp/app/risks/webapp/Component.js
+++ cpapp/app/risks/webapp/Component.js
@@ -1,6 +1,7 @@
 sap.ui.define(
     ["sap/fe/core/AppComponent"],
-    function (Component) {
+    function (Component)
+    {
         "use strict";
 
         return Component.extend("ns.risks.Component", {
