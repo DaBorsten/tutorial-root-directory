bfa6b5433f2c35e7cde59abb8087d9f366fbd201
Boas Rautmann <boas_rautmann@web.de>
2023-12-19 06:57:46 +0100

    deploy risk data

---
+++ cpapp/app/risks/dist/Component-dbg.js
@@ -0,0 +1,13 @@
+sap.ui.define(
+    ["sap/fe/core/AppComponent"],
+    function (Component)
+    {
+        "use strict";
+
+        return Component.extend("ns.risks.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
